95b1697a0368a0904c70e73aec213f946cd2983d
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-05 09:57:40 +0200

    Initial Commit

---
+++ Moonshoot-ios17/Moonshoot_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshoot_ios17App.swift
+//  Moonshoot-ios17
+//
+//  Created by Alin RADU on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Moonshoot_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
